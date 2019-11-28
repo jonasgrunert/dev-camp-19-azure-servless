2aba3d18ee69dcce6986d84988a203ae453dc8f4
jonasgrunert <jonas.grunert@bayer.com>
2019-11-28 17:01:02 +0100

    Intermediate commit

--- sentiment/__init__.py
+++ sentiment/__init__.py
@@ -1,24 +1,35 @@
 import logging
-
+import json
 import azure.functions as func
-
+import requests
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
+    token = req.headers.get('Authorization')
+    if not token:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+            json.dumps({
+                'code': 'Unauthorized',
+                'message': 'No Authorization header found'
+            }),
+            headers={
+                'Content-Type': 'application/json'
+            },
+            status_code=403
         )
+    
+    # provider = req.params.get('provider')
+    # if not provider:
+    #     return func.HttpResponse(
+    #         "Please pass a provider",
+    #         status_code=400
+    #     )
+    r = requests.get('https://graph.facebook.com/v5.0/me/posts?fields=message&access_token='+token)
+    msg = []
+    for item in r.json()["data"]:
+        try:
+            msg.append(item["message"])
+        except:
+            pass
+    return func.HttpResponse(
+        json.dumps(msg)
+    )
