03cbb55ee6d54bccc6451dde374dc5eba6fd8aac
Lucas Merker <lucas.merker@bayer.com>
2019-12-05 08:56:55 +0100

    setup on linux with flair

--- sentiment/__init__.py
+++ sentiment/__init__.py
@@ -2,6 +2,9 @@ import logging
 import json
 import azure.functions as func
 import requests
+from flair.models import TextClassifier
+from flair.data import Sentence
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     token = req.headers.get('Authorization')
@@ -16,20 +19,35 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             },
             status_code=403
         )
-    
+
     # provider = req.params.get('provider')
     # if not provider:
     #     return func.HttpResponse(
     #         "Please pass a provider",
     #         status_code=400
     #     )
-    r = requests.get('https://graph.facebook.com/v5.0/me/posts?fields=message&access_token='+token)
+    r = requests.get(
+        'https://graph.facebook.com/v5.0/me/posts?fields=message&access_token='+token)
     msg = []
     for item in r.json()["data"]:
         try:
             msg.append(item["message"])
+            try:
+                classifier = TextClassifier.load('en-sentiment')
+                sentence = Sentence(msg[0])
+                classifier.predict(sentence)
+                # print sentence with predicted labels
+                print('Sentence above is: ', sentence.labels)
+            except:
+                pass
         except:
             pass
     return func.HttpResponse(
-        json.dumps(msg)
+        status_code=200,
+        headers={
+            'Content-Type': 'application/json'
+        },
+        json.dumps({
+            sentence.labels
+        })
     )
